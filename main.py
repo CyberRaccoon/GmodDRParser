32e6a13cdea47122f386312809562dcd577e0828
satohashi <yenot.raketa@gmail.com>
2021-12-20 03:51:59 +0300

    Improved: classes parsed too!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+import os
+import errno
 import json
 from requests import get as get_request
 from lxml import etree
@@ -21,85 +23,171 @@ class Entry:
     def prepare_args(self) -> str:
         result = ''
         for arg in self.args:
-            result += f"--- @param {arg[2].text} {arg[1].text}"
-            if arg != self.args[-1]:
-                result += '\n'
-
+            result += f"\n--- @param {arg[2].text} {arg[1].text}"
         return result
 
     def prepare_description(self) -> str:
-        data = '--- '
+        data = ''
         for description in self.desc:
-            data += description.text
-
+            if description.text:
+                description.text.strip()
+            data += f'\n--- {description.text}'
         return data
 
     @property
     def is_deprecated(self):
         if self.deprecated:
-            return '--- @deprecated'
+            return '\n--- @deprecated'
         return ''
 
     def prepare_result(self) -> str:
 
         if not self.returns:
             return ''
-        data = '--- @return '
-
+        data = '\n--- @return '
         for result in self.returns:
             data += f'{result.text}'
             if result != self.returns[-1]:
                 data += ', '
-
         return data
 
     def entry_name(self) -> str:
         result = 'function '
         result += self.func + '('
         for argname in self.args:
-            if argname != self.args[-1]:
-                result += argname[2].text + ', '
+            if argname[2].text == None:
+                result += 'notNamed'
             else:
-                result += argname[2].text + ') end'
+                result += argname[2].text
 
+            if argname != self.args[-1]:
+                result += ', '
+        result += ') end'
         return result
 
     def __str__(self) -> str:
-        return f'--- {self.side}' \
+        return f'\n--- {self.side}' \
                f'\n--- ' \
                f'\n{self.prepare_description()}' \
                f'\n--- ' \
-               f'\n{self.prepare_args()}' \
-               f'\n{self.prepare_result()}' \
-               f'\n{self.is_deprecated}' \
-               f'\n{self.entry_name()}'
+               f'{self.prepare_args()}' \
+               f'{self.prepare_result()}' \
+               f'{self.is_deprecated}' \
+               f'\n{self.entry_name()}\n'
+
+def parse_class(cls_name: str):
+    global cache
+
+    if cls_name in cache:
+        cached = open(f"cache/{cls_name}", 'r')
+        html = cached.read()
+        cached.close()
+    else:
+        cached = open(f"cache/{cls_name}", 'w+')
+        response = get_request(base_uri + cls_name)
+        html = response.text
+        cached.write(response.text)
+    etree_fromstring = etree.HTML(html)
+    class_desc = ''
+    for desc in etree_fromstring.xpath('//div[@class="section"]/p'):
+        class_desc += desc.text
+
+    return f'--- {class_desc}\n---\n---\n---@class {cls_name}\n'
+
+def parsing(data: dict, type: str):
+    global cache
+
+    if data['address'] in cache:
+        cached = open(f"cache/{data['address']}", 'r')
+        html = cached.read()
+        cached.close()
+    else:
+        cached = open(f"cache/{data['address']}", 'w+')
+        response = get_request(base_uri + data['address'])
+        html = response.text
+        cached.write(response.text)
+
+    if type == 'globals':
+        function_name = data['address'].replace('Global.', '')
+    elif type == 'classes':
+        function_name = data['address']
+    else:
+        raise Exception('Not expected type!')
+
+    etree_fromstring = etree.HTML(html)
+
+    return prepare_entry(etree_fromstring, function_name)
+
+
+def prepare_entry(etree, function_name):
+    global entry
+
+    deprecated = len(etree.xpath('//div[@class="deprecated"]')) > 0
+    description = etree.xpath('//div[@class="description_section function_description section"]/p')
+    side = etree.xpath('//div[@class="function_line"]/a[@class="realm_icon"]')[0].attrib['title']
+    args = etree.xpath('//div[@class="function_arguments section"]/div')
+    results = etree.xpath('//div[@class="function_returns section"]/div/a')
+
+    return Entry(side, description, args, function_name, results, deprecated)
+
+
+# //div[@class="section"and position()=6]/details[@class="level1" and position()= 3]/ul/li (libs)
+
+def class_to_file(cls):
+    result = ''
+    print(cls)
+    current = ''
+    for class_name in cls:
+        class_file = open(f'class/{class_name}.lua', 'w+')
+        class_file.write(parse_class(class_name))
+        class_methods = cls[class_name]
+        for method in class_methods:
+            result = result + method.__str__()
+        class_file.write(result)
+        current = class_name
+        class_file.close()
+    cls.pop(current)
+    return cls
 
 
 if __name__ == '__main__':
-
+    try:
+        os.mkdir('class')
+        os.mkdir('cache')
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+        pass
+    cache = os.listdir('cache')
     base_uri = 'https://wiki.facepunch.com/gmod/'
 
-    pagelisting = open('pagelisting', 'r')
+    classes = {}
+    exceptions = ['CS:S_Kill_Icons', 'C_Lua:_Functions', 'gamemodes:PropHunt', 'Tables:_Bad_Habits']
+
+    page_listing = open('pagelisting', 'r')
     g_globals = open('globals.lua', 'w+')
 
-    all_pages = json.load(pagelisting)
+    all_pages = json.load(page_listing)
     for page in all_pages:
-
         # Parsing global
         if 'Global.' in page['address']:
-            response = get_request(base_uri + page['address'])
-            functionName = page['address'].replace('Global.', '')
-
-            etree_fromstring = etree.HTML(response.text)
-
-            deprecated = len(etree_fromstring.xpath('//div[@class="deprecated"]')) > 0
-            description = etree_fromstring.xpath('//div[@class="description_section function_description section"]/p')
-            side = etree_fromstring.xpath('//div[@class="function_line"]/a[@class="realm_icon"]')[0].attrib['title']
-            args = etree_fromstring.xpath('//div[@class="function_arguments section"]/div')
-            results = etree_fromstring.xpath('//div[@class="function_returns section"]/div/a')
-            entry = Entry(side, description, args, functionName, results, deprecated)
-
+            print('global', page['address'])
+            entry = parsing(page, 'globals')
             g_globals.write(entry.__str__())
 
+        # Parsing classes
+        if re.fullmatch(r'[a-zA-z_]+:[a-zA-z_]+', page['address']):
+            if page['address'] in exceptions:
+                # skip topic page
+                continue
+
+            class_name = page['address'].split(':')[0]
+            print(class_name, page['address'])
+            if class_name not in classes:
+                if classes:
+                    classes = class_to_file(classes)
+                classes[class_name] = []
+            classes[class_name].append(parsing(page, 'classes'))
+
     g_globals.close()
-    pagelisting.close()
+    page_listing.close()
